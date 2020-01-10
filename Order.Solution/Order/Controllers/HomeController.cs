9fd946899b4146595a9157ba9e9f5977cadb19de
Jamila Aliyeva <aliyeva852@gmail.com>
2020-01-10 09:11:33 -0800

    be working on setting up routes for Order class.

--- Order.Solution/Order/Controllers/HomeController.cs
+++ Order.Solution/Order/Controllers/HomeController.cs
@@ -1,22 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;  
-using Template.Models;
+using Pierre.Models;
 
-namespace Template.Controllers
+namespace Pierre.Controllers
 {
   public class HomeController : Controller
   {
-      [Route("/template")]//First main user page;
-      public ActionResult Template() { return View(); }
-
-      [Route("/second")]//
-      public ActionResult Second(string test1,string test2) 
-      {
-          Class newClass = new Class(test1, test2);
-          //You can instantiate classes here to have them render on
-         // your last page
-
-           return View(newClass); }
-
+   [Route("/")]
+   public ActionResult Index()
+   {
+     return View();
+   }
   }
 }
---
+++ Order.Solution/Order/Controllers/OrderController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+using Pierre.Models;
+using System.Collections.Generic;
+
+namespace Pierre.Controllers
+{
+  public class ItemsController : Controller
+  {
